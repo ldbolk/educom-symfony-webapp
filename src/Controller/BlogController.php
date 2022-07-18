fc5f9e737bf846953dd1dafa7bdf0d16faff9cfc
ldbolk <ldbolk@ziggo.nl>
2022-07-18 15:45:35 +0200

    Meertaligheid

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Psr\Log\LoggerInterface;
+
 
 #[Route('/blog')]
 class BlogController extends AbstractController
@@ -19,7 +23,11 @@ class BlogController extends AbstractController
     }
 
     #[Route('/show/{id}', name: 'blog_show')]
-    public function show($id = 1) {
+    public function show($id = 1, LoggerInterface $logger) {
+
+        $logger->info("info Message");
+        $logger->Warning("Warning Message");
+        $logger->error("De waarde van id is: $id");
         dd($id);
     }
     
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,7 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HtppFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-// use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route("/")]
 class HomepageController extends AbstractController
@@ -22,5 +23,19 @@ class HomepageController extends AbstractController
     public function backhome() {
         return $this->redirectToRoute('homepage');
     }
-    
+
+    /**
+     * @Route({
+     * "en": "/contact-us",
+     * "nl": "/neem-contact-op"
+     * }, name="contact")
+     */
+    public function contact(Request $request) {
+        $locale = $request->getLocale();
+        $msg = "This page is in English";
+        if($locale == "nl") {
+            $msg = "Deze pagina is in het Nederlands";
+        }
+        return new Response("<html><body>$msg</body></html>");
+    }
 }
